e9dc49076b696a98b00816e13174fdf168ef23b1
nabura <wildcard6808@gmail.com>
2018-12-20 13:23:56 +0900

    Initialize repository

---
+++ Assets/GameResult.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameResult : MonoBehaviour {
+    private int highScore;
+    public Text resultTime;
+    public Text bestTime;
+    public GameObject parts;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScore");
+        }
+        else
+        {
+            highScore = 999;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GoalArea.goal)
+        {
+            parts.SetActive(true);
+            int result = Mathf.FloorToInt(Timer.time);
+            resultTime.text = "ResultTime " + result;
+            bestTime.text = "BestTime " + highScore;
+
+            if(highScore > result)
+            {
+                PlayerPrefs.SetInt("HighScore", result);
+            }
+        }
+	}
+
+    public void OnRetry()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public static float time;
+
+	// Use this for initialization
+	void Start () {
+        time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GoalArea.goal == false)
+        {//ゴールしていないとき
+            time += Time.deltaTime;     //時間経過
+        }
+
+        //時間の表示
+        int t = Mathf.FloorToInt(time);
+        Text uiText = GetComponent<Text>();
+        uiText.text = "Time:" + t.ToString();
+	}
+}
